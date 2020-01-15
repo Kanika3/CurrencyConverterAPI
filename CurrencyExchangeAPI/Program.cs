30e75f8b049461c1e36f8e418202ddc04fef1d52
Kanika.Arora <kanika3arora@gmail.com>
2020-01-15 11:19:09 +0000

    Added test for exchange rate API

--- CurrencyExchangeAPI/Program.cs
+++ CurrencyExchangeAPI/Program.cs
@@ -19,6 +19,7 @@ namespace CurrencyExchangeAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("https://localhost:44301");
     }
 }
---
+++ CurrencyExchangeAPITests/Controllers/ExchangeRatesControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyExchangeAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class ExchangeRatesControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            string from = "USD";
+            string to = "EUR";
+
+            var controller = new ExchangeRatesController();
+            var result = controller.Get(from, to);
+            Assert.AreEqual(0.90, result);
+        }
+    }
+}
